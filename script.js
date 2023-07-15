0ae95645a09465a9d7db43027662f68a9ad20ea3
Shivani verma <shivani.vr.verma@gmail.com>
2023-07-16 01:02:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let colors = [
+    {bg_color:'#E58595'},
+    {bg_color:'#E5727E'},
+    {bg_color:'#E87C79'},
+    {bg_color:'#BC5761'},
+    {bg_color:'#C9365F'},
+    {bg_color:'#B01F57'},
+    {bg_color:'#B77266'},
+    {bg_color:'#9E6144'},
+    {bg_color:'#9B5F53'},
+]
+let inpt1 = document.querySelector('#inputSeclor1')
+let inpt2 = document.querySelector('#inputSeclor2')
+let inpt3 = document.querySelector('#inputSeclor3')
+let inpt4 = document.querySelector('#inputSeclor4')
+let inpt5 = document.querySelector('#inputSeclor5')
+let inpt6 = document.querySelector('#inputSeclor6')
+let inpt7 = document.querySelector('#inputSeclor7')
+let inpt8 = document.querySelector('#inputSeclor8')
+let inpt9 = document.querySelector('#inputSeclor9')
+let inpt10 = document.querySelector('#inputSeclor10')
+
+
+
+
+// shop the feed
+
+let shpt_feed = document.querySelectorAll('.card-10')
+shpt_feed.forEach(function(e){
+    let img_var = e.childNodes[1]
+    img_var.addEventListener('mouseenter',function(dets){
+        
+    })
+})
